0b70071d5a9c10be20e7f713dcd6f42ef527b32a
Andreas Schmidt <andreas.schmidt.lth@t-online.de>
2022-01-10 14:57:19 +0100

    Added extended Calculator feature. Programmed first three functions.

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 'use strice'
 
+
 let inputOne = '';
 let action = '';
 let valueOne = 0;
 let valueTwo = 0;
 let result = 0;
 
-const display = document.getElementById('display');
 
 const refreshDisplay = (value) => {
-    display.innerHTML = value;
+    document.getElementById('display').innerHTML = value;
+}
+
+const adjustPadding = (value)=>{
+    document.getElementById('display').style.paddingRight = value;
+}
+
+const resetAll = () => {
+    inputOne = '0';
+    action = '';
+    valueOne = 0;
+    valueTwo = 0;
+    result = 0;
+    refreshDisplay(inputOne);
 }
 
 // const addDots = (str) => {
@@ -30,6 +43,20 @@ const refreshDisplay = (value) => {
 //         return str;
 //     }
 // }
+
+document.getElementById('switch').checked = false;
+
+document.getElementById('switch').addEventListener('change',() =>{
+    if(document.getElementById('switch').checked){
+        document.getElementById('calculator').innerHTML = extended; 
+        adjustPadding('4rem');
+        resetAll();
+    } else {
+        document.getElementById('calculator').innerHTML = simple;
+        adjustPadding('9rem');
+        resetAll();
+    }
+});
 
 const pressEquals = () => {
     toggleAnimation('btn-=', 'animation-3');
@@ -231,6 +258,151 @@ const pressComma = () => {
     refreshDisplay(inputOne);
 }
 
+// Function keys
+
+const funk1 = () => {
+    toggleAnimation('btn-funk1', 'animation-4');
+}
+
+const funk2 = () => {
+    toggleAnimation('btn-funk2', 'animation-4');
+}
+
+const funk3 = () => {
+    toggleAnimation('btn-funk3', 'animation-4');
+}
+
+const funk4 = () => {
+    toggleAnimation('btn-funk4', 'animation-4');
+}
+
+const funk5 = () => {
+    toggleAnimation('btn-funk5', 'animation-4');
+}
+
+const funk6 = () => {
+    toggleAnimation('btn-funk6', 'animation-4');
+}
+
+const funk7 = () => {
+    toggleAnimation('btn-funk7', 'animation-4');
+}
+
+const funk8 = () => {
+    // x hoch 2
+    toggleAnimation('btn-funk8', 'animation-4');
+    if(inputOne !== '0'){
+         inputOne = Number.parseFloat(inputOne)*Number.parseFloat(inputOne)
+    } else {
+        inputOne = '0';
+    }
+    refreshDisplay(inputOne);
+}
+
+const funk9 = () => {
+    // x hoch 3
+    toggleAnimation('btn-funk9', 'animation-4');
+    if(inputOne !== '0'){
+         inputOne = Number.parseFloat(inputOne)*Number.parseFloat(inputOne)*Number.parseFloat(inputOne)
+    } else {
+        inputOne = '0';
+    }
+    refreshDisplay(inputOne);
+}
+
+const funk10 = () => {
+    toggleAnimation('btn-funk10', 'animation-4');
+}
+
+const funk11 = () => {
+    toggleAnimation('btn-funk11', 'animation-4');
+}
+
+const funk12 = () => {
+    toggleAnimation('btn-funk12', 'animation-4');
+}
+
+const funk13 = () => {
+    toggleAnimation('btn-funk13', 'animation-4');
+}
+
+const funk14 = () => {
+    toggleAnimation('btn-funk14', 'animation-4');
+}
+
+const funk15 = () => {
+    toggleAnimation('btn-funk15', 'animation-4');
+}
+
+const funk16 = () => {
+    toggleAnimation('btn-funk16', 'animation-4');
+}
+
+const funk17 = () => {
+    toggleAnimation('btn-funk17', 'animation-4');
+}
+
+const funk18 = () => {
+    toggleAnimation('btn-funk18', 'animation-4');
+}
+
+const funk19 = () => {
+    toggleAnimation('btn-funk19', 'animation-4');
+}
+
+const funk20 = () => {
+    toggleAnimation('btn-funk20', 'animation-4');
+}
+
+const funk21 = () => {
+    toggleAnimation('btn-funk21', 'animation-4');
+}
+
+const funk22 = () => {
+    toggleAnimation('btn-funk22', 'animation-4');
+}
+
+const funk23 = () => {
+    toggleAnimation('btn-funk23', 'animation-4');
+}
+
+const funk24 = () => {
+    toggleAnimation('btn-funk24', 'animation-4');
+}
+
+const funk25 = () => {
+    toggleAnimation('btn-funk25', 'animation-4');
+}
+
+const funk26 = () => {
+    toggleAnimation('btn-funk26', 'animation-4');
+}
+
+const funk27 = () => {
+    toggleAnimation('btn-funk27', 'animation-4');
+}
+
+const funk28 = () => {
+    toggleAnimation('btn-funk28', 'animation-4');
+}
+
+const funk29 = () => {
+    // pi
+    toggleAnimation('btn-funk29', 'animation-4');
+    if(inputOne !== '0'){
+        inputOne = Math.PI;
+   } else {
+       inputOne = '0';
+   }
+   inputOne = Math.PI.toString();
+   refreshDisplay(inputOne);
+}
+
+
+const funk30 = () => {
+    toggleAnimation('btn-funk30', 'animation-4');
+}
+
 document.addEventListener('keydown', (event) => {
     document.activeElement.blur();
     // console.log(event.key);
@@ -306,3 +478,101 @@ const toggleAnimation = (id,kind) =>{
         document.getElementById(id).classList.toggle(kind);
     },150);
 }
+
+const simple = `    <div id="calculator">
+<div class="row">
+    <div id="display">0</div>
+</div>
+<div class="row">
+    <button class="btn btn-style-1" tapindex="-1" focusable="false" id="btn-AC"  onclick="pressAC()">AC</button>
+    <button class="btn btn-style-1" tapindex="-1" id="btn-Change"  onclick="pressChangeSign()">+/-</button>
+    <button class="btn btn-style-1" tapindex="-1" id="btn-%"  onclick="pressPercent()">%</button>
+    <button class="btn btn-style-4" tapindex="-1" id="btn-/"  onclick="pressDivide()">/</button>
+</div>
+<div class="row">
+    <button class="btn btn-style-2" tapindex="-1" id="btn-7"  onclick="press7()">7</button>
+    <button class="btn btn-style-2" tapindex="-1" id="btn-8"  onclick="press8()">8</button>
+    <button class="btn btn-style-2" tapindex="-1" id="btn-9"  onclick="press9()">9</button>
+    <button class="btn btn-style-4" tapindex="-1" id="btn-*"  onclick="pressMultiply()">X</button>
+</div>
+<div class="row">
+    <button class="btn btn-style-2" tapindex="-1" id="btn-4"  onclick="press4()">4</button>
+    <button class="btn btn-style-2" tapindex="-1" id="btn-5"  onclick="press5()">5</button>
+    <button class="btn btn-style-2" tapindex="-1" id="btn-6"  onclick="press6()">6</button>
+    <button class="btn btn-style-4" tapindex="-1" id="btn--"  onclick="pressMinus()">-</button>
+</div>
+<div class="row">
+    <button class="btn btn-style-2" tapindex="-1" id="btn-1" onclick="press1()">1</button>
+    <button class="btn btn-style-2" tapindex="-1" id="btn-2"  onclick="press2()">2</button>
+    <button class="btn btn-style-2" tapindex="-1" id="btn-3"  onclick="press3()">3</button>
+    <button class="btn btn-style-4" tapindex="-1" id="btn-+"  onclick="pressAdd()">+</button>
+</div>
+<div class="row">
+    <button class="btn btn-style-3" tapindex="-1" id="btn-0"  onclick="press0()">0</button>
+    <button class="btn btn-style-2" tapindex="-1" id="btn-,"  onclick="pressComma()">,</button>
+    <button class="btn btn-style-4" tapindex="-1" id="btn-="  onclick="pressEquals()">=</button>
+</div>
+</div>`;
+
+const extended = `        <div class="row">
+<div id="display">0</div>
+</div>
+<div class="row">
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk1" onclick="funk1()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk2" onclick="funk2()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk3" onclick="funk3()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk4" onclick="funk4()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk5" onclick="funk5()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk6" onclick="funk6()">-_-</button>
+<button class="btn btn-style-1" tapindex="-1" id="btn-AC" onclick="pressAC()">AC</button>
+<button class="btn btn-style-1" tapindex="-1" id="btn-Change"  onclick="pressChangeSign()">+/-</button>
+<button class="btn btn-style-1" tapindex="-1" id="btn-%"  onclick="pressPercent()">%</button>
+<button class="btn btn-style-4" tapindex="-1" id="btn-/"  onclick="pressDivide()">/</button>
+</div>
+<div class="row">
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk7" onclick="funk7()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk8" onclick="funk8()">x²</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk9" onclick="funk9()">x³</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk10" onclick="funk10()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk11" onclick="funk11()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk12" onclick="funk12()">-_-</button>
+<button class="btn btn-style-2" tapindex="-1" id="btn-7"  onclick="press7()">7</button>
+<button class="btn btn-style-2" tapindex="-1" id="btn-8"  onclick="press8()">8</button>
+<button class="btn btn-style-2" tapindex="-1" id="btn-9"  onclick="press9()">9</button>
+<button class="btn btn-style-4" tapindex="-1" id="btn-*"  onclick="pressMultiply()">X</button>
+</div>
+<div class="row">
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk13" onclick="funk13()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk14" onclick="funk14()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk15" onclick="funk15()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk16" onclick="funk16()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk17" onclick="funk17()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk18" onclick="funk18()">-_-</button>
+<button class="btn btn-style-2" tapindex="-1" id="btn-4"  onclick="press4()">4</button>
+<button class="btn btn-style-2" tapindex="-1" id="btn-5"  onclick="press5()">5</button>
+<button class="btn btn-style-2" tapindex="-1" id="btn-6"  onclick="press6()">6</button>
+<button class="btn btn-style-4" tapindex="-1" id="btn--"  onclick="pressMinus()">-</button>
+</div>
+<div class="row">
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk19" onclick="funk19()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk20" onclick="funk20()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk21" onclick="funk21()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk22" onclick="funk22()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk23" onclick="funk23()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk24" onclick="funk24()">-_-</button>
+<button class="btn btn-style-2" tapindex="-1" id="btn-1" onclick="press1()">1</button>
+<button class="btn btn-style-2" tapindex="-1" id="btn-2"  onclick="press2()">2</button>
+<button class="btn btn-style-2" tapindex="-1" id="btn-3"  onclick="press3()">3</button>
+<button class="btn btn-style-4" tapindex="-1" id="btn-+"  onclick="pressAdd()">+</button>
+</div>
+<div class="row">
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk25" onclick="funk25()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk26" onclick="funk26()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk27" onclick="funk27()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk28" onclick="funk28()">-_-</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk29" onclick="funk29()">π</button>
+<button class="btn btn-style-5" tapindex="-1" id="btn-funk30" onclick="funk30()">-_-</button>
+<button class="btn btn-style-3" tapindex="-1" id="btn-0"  onclick="press0()">0</button>
+<button class="btn btn-style-2" tapindex="-1" id="btn-,"  onclick="pressComma()">,</button>
+<button class="btn btn-style-4" tapindex="-1" id="btn-="  onclick="pressEquals()">=</button>
+</div>`
